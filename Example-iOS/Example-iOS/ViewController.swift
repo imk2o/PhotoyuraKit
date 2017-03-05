5fcd0ba5ac71717a8357974324e6517304285c81
imk2o <imk2o@icloud.com>
2017-03-05 11:44:52 +0900

    Add iOS example project

---
+++ Example-iOS/Example-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by k2o on 2017/03/05.
+//  Copyright © 2017年 imk2o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
