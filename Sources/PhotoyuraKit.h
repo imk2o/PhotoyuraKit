d33b03abdd9ebfdc745dd0538c7638cabb8a5853
imk2o <imk2o@icloud.com>
2017-03-05 11:28:04 +0900

    initialize projects

---
+++ Sources/PhotoyuraKit.h
@@ -0,0 +1,17 @@
+//
+//  PhotoyuraKit.h
+//  PhotoyuraKit
+//
+//  Created by k2o on 2017/03/05.
+//
+//
+
+@import Foundation;
+
+//! Project version number for PhotoyuraKit.
+FOUNDATION_EXPORT double PhotoyuraKitVersionNumber;
+
+//! Project version string for PhotoyuraKit.
+FOUNDATION_EXPORT const unsigned char PhotoyuraKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PhotoyuraKit_iOS/PublicHeader.h>
